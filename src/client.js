3a98eef38ce7c3c28725e1b8440eb46a7e8d97c9
iamso <so@dev.so>
2016-11-18 11:55:25 +0100

    rewrite client with es6

--- src/client.js
+++ src/client.js
@@ -1,137 +1,108 @@
 
-;(function (root, factory) {
-  'use strict';
-
-  if (/^f/.test(typeof define) && define.amd) {
-    define('msg', [], factory);
-  }
-  else if (/^o/.test(typeof exports)) {
-    module.exports = factory();
-  }
-  else {
-    root.Msg = factory();
-  }
-})(!/^u/.test(typeof window) ? window : this, function() {
-  'use strict';
+export default class Msg {
 
   /**
    * Msg constructor
-     * @param  {String}   channel   - channel to join
-     * @param  {Function} onmsg     - onmessage handler
-     * @param  {Function} onerror   - onerror handler
-     * @param  {Object}   [context] - context for event handlers
-     * @param  {String}   [url]     - websocket server url
-   * @return {Object}               - Msg instance
+   * @param  {String}   channel   - channel to join
+   * @param  {Function} onmsg     - onmessage handler
+   * @param  {Function} onerror   - onerror handler
+   * @param  {Object}   [context] - context for event handlers
+   * @param  {String}   [url]     - websocket server url
    */
-  var Msg = function(channel, onmsg, onerror, context, url) {
-    this.url = url || '<%=url%>';
+  constructor(channel, onmsg, onerror, context = this, url = '<%=url%>') {
+    this.url = url;
     this.channel = channel;
     this.onmsg = onmsg;
     this.onerror = onerror;
-    this.context = context || this;
+    this.context = context;
     this.q = [];
-    this._init();
-  };
+    this::init();
+  }
 
   /**
-   * Msg prototype
-   * @type {Object}
+   * send function
+   * Exposed send function
+   * @param  {Object|String} data - data to send
    */
-  Msg.prototype = {
-
-    /**
-     * _init function
-     * Initialize the websocket
-     */
-    _init: function() {
-      // return if WebSocket is not supported
-      if (!WebSocket) {
-        return;
-      }
-      var _this = this;
-      _this.ws = new WebSocket(_this.url, _this.channel);
-      _this.ws.onopen = _this._open.bind(_this);
-      _this.ws.onmessage = _this._message.bind(_this);
-      _this.ws.onerror = _this._error.bind(_this);
-      _this.ws.onclose = _this._close.bind(_this);
-    },
-
-    /**
-     * _open function
-     * Internal onopen handler
-     * @param  {Object} e - event
-     */
-    _open: function(e) {
-      var _this = this;
-      while(_this.q.length) {
-        try {
-          _this.ws.send(_this.q.shift());
-        }
-        catch(e) {}
-      }
-    },
-
-    /**
-     * _message function
-     * Internal onmessage handler
-     * @param  {Object} e - event
-     */
-    _message: function(e) {
-      var data = e.data;
+  send(data) {
+    try {
+      data = JSON.stringify(data);
+    }
+    catch(e) {}
+    if (this.ws.readyState === 1) {
       try {
-        data = JSON.parse(data);
+        this.ws.send(data);
       }
-      catch(e) {}
-      /^f/.test(typeof this.onmsg) && this.onmsg.apply(this.context, [data, e, this]);
-    },
-
-    /**
-     * _error function
-     * Internal onerror handler
-     * @param  {Object} e - event
-     */
-    _error: function(e) {
-      /^f/.test(typeof this.onerror) && this.onerror.apply(this.context, [e, this]);
-    },
-
-    /**
-     * _close function
-     * Internal onclose handler
-     * @param  {Object} e - event
-     */
-    _close: function(e) {
-      clearTimeout(this.timeout);
-      this.timeout = setTimeout(this._init.bind(this), 5000);
-    },
-
-    /**
-     * send function
-     * Exposed send function
-     * @param  {Object|String} data - data to send
-     */
-    send: function(data) {
-      try {
-        data = JSON.stringify(data);
-      }
-      catch(e) {}
-      if (this.ws.readyState === 1) {
-        try {
-          this.ws.send(data);
-        }
-        catch(e) {
-          this.q.push(data);
-        }
-      }
-      else {
+      catch(e) {
         this.q.push(data);
       }
-    },
-  };
+    }
+    else {
+      this.q.push(data);
+    }
+  }
 
-  /**
-   * Return Msg
-   * @type {Object}
-   */
-  return Msg;
+}
 
-});
+/**
+ * _init function
+ * Initialize the websocket
+ */
+function init() {
+  // return if WebSocket is not supported
+  if (!WebSocket) {
+    return;
+  }
+  this.ws = new WebSocket(this.url, this.channel);
+  this.ws.onopen = onopen.bind(this);
+  this.ws.onmessage = onmessage.bind(this);
+  this.ws.onerror = onerror.bind(this);
+  this.ws.onclose = onclose.bind(this);
+}
+
+/**
+ * _open function
+ * Internal onopen handler
+ * @param  {Object} e - event
+ */
+function onopen(e) {
+  while(this.q.length) {
+    try {
+      this.ws.send(this.q.shift());
+    }
+    catch(e) {}
+  }
+}
+
+/**
+ * _message function
+ * Internal onmessage handler
+ * @param  {Object} e - event
+ */
+function onmessage(e) {
+  let data = e.data;
+  try {
+    data = JSON.parse(data);
+  }
+  catch(e) {}
+  /^f/.test(typeof this.onmsg) && this.onmsg.apply(this.context, [data, e, this]);
+}
+
+/**
+ * _error function
+ * Internal onerror handler
+ * @param  {Object} e - event
+ */
+function onerror(e) {
+  /^f/.test(typeof this.onerror) && this.onerror.apply(this.context, [e, this]);
+}
+
+/**
+ * _close function
+ * Internal onclose handler
+ * @param  {Object} e - event
+ */
+function onclose(e) {
+  clearTimeout(this.timeout);
+  this.timeout = setTimeout(this._init.bind(this), 5000);
+}
